b17bed4c95578b2bc8ed5600626def9292f5289c
sam9095 <68658856+sam9095@users.noreply.github.com>
2020-08-28 19:10:42 +0000

    Modified

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 
-
-// state data for 3 counters
-const data = [
-  { id: 1, value: 0 },
-  { id: 2, value: 0 },
-  { id: 3, value: 0 },
-];
-
 // Counter Component
 class Counter extends Component {
   render() {
-    const { value } = this.props;
+    const { value, increment, decrement } = this.props;
     return (
       <div className="counter">
         <b>{value}</b>
-        <div className="counter-controls">
-          <button className="button is-danger is-small">-</button>
-          <button className="button is-success is-small">+</button>
+        <div className="counter-marks">
+          <button onClick={decrement} className="button is-high is-good">-</button>
+          <button onClick={increment} className="button is-low is-good">+</button>
         </div>
+      </div>
+    );
+  }
+}
+
+class Total extends Component {
+  totalValue = () => this.props.getData.reduce((x, y) => x + y.value, 0)
+  render() {
+    return (
+      <div className="counter">
+        <b>Total Value: {this.totalValue()}</b>
       </div>
     );
   }
@@ -27,14 +30,38 @@ class Counter extends Component {
 class App extends Component {
   constructor(props, context) {
     super(props, context);
+    this.state = {
+      data: [
+        { id: 1, value: 0 },
+        { id: 2, value: 0 },
+        { id: 3, value: 0 },
+        { id: 4, value: 0 },
+      ]
+    }
   }
 
+  onIncrement = (id) => {
+    this.setState(prevState => ({
+      data: prevState.data.map(a => a.id === id ? { id: a.id, value: a.value++ } : a)
+    }));
+  }
+
+  onDecrement = (id) => {
+    this.setState(prevState => ({
+      data: prevState.data.map(a => a.id === id ? { id: a.id, value: a.value-- } : a)
+    }));
+  }
   render() {
     return (
       <div>
-        {data.map((counter) => (
-          <Counter key={counter.id} value={counter.value} />
+        {this.state.data.map((counter) => (
+          <Counter
+            increment={() => this.onIncrement(counter.id)}
+            decrement={() => this.onDecrement(counter.id)}
+            key={counter.id} value={counter.value} />
         ))}
+
+        <Total getData={this.state.data} />
       </div>
     );
   }
